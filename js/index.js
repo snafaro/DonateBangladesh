4fff20f0397e284f25330cf886d7e274c087811d
shahriar nafis <snafaro02@gmail.com>
2025-06-06 01:24:18 +0600

    history section functionality complete

--- js/index.js
+++ js/index.js
@@ -6,12 +6,22 @@ function clickEvent (id) {
     const inputValue = parseInt(e.target.parentNode.querySelector('input').value);
     const donationValue = parseInt(e.target.parentNode.querySelector('span').innerText);
     const myBalance = parseInt(document.getElementById('balance').innerText);
+    const heading = e.target.parentNode.querySelector('h2').innerText;
+    const time = new Date();
 
     if((inputValue > 0 && inputValue < myBalance) && typeof inputValue !== "string"){
         const donationValueUpdate = donationValue + inputValue;
         const myBalanceUpdate = myBalance - inputValue;
         e.target.parentNode.querySelector('span').innerText = donationValueUpdate; 
-        document.getElementById('balance').innerText = myBalanceUpdate; 
+        document.getElementById('balance').innerText = myBalanceUpdate;
+        let div = document.createElement('div');
+        div.classList.add("border","border-[#c2c2c2]","p-[20px]", "rounded-md", "mt-10", "w-1/2");
+        div.innerHTML = `
+        <h1 class="card-title">${inputValue} Taka is donated for ${heading}<h1>
+        <p>${time}</p>
+        `;
+        document.getElementById('history-card-container').appendChild(div);
+        document.getElementById('my_modal_4').showModal();
     }
     else{
         alert("Please Provide Valid Amount");
@@ -23,3 +33,22 @@ function clickEvent (id) {
 clickEvent("donate-noakhali");
 clickEvent("donate-feni");
 clickEvent("donate-quota");
+
+document.getElementById('history-btn').addEventListener('click', e=>{
+    e.target.classList.add('bg-[#B4F461]');
+    document.getElementById('donate-btn').classList.remove('bg-[#B4F461]');
+    document.getElementById('history-card-container').classList.add('flex');
+    document.getElementById('donate-card-container').classList.add('hidden');
+    document.getElementById('history-card-container').classList.remove('hidden');
+    
+});
+
+document.getElementById('donate-btn').addEventListener('click', e=>{
+    e.target.classList.add('bg-[#B4F461]');
+    document.getElementById('history-btn').classList.remove('bg-[#B4F461]');
+    document.getElementById('history-card-container').classList.add('hidden');
+    document.getElementById('donate-card-container').classList.remove('hidden');
+    document.getElementById('history-card-container').classList.remove('flex');
+    
+})
+
